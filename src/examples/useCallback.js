d0ac27b3605202b23c79c11066676bcc2fbe2300
sandrig <sandrig.dev@gmail.com>
2020-08-26 19:27:01 +0300

    feat: add useCallback hook example

---
+++ src/examples/useCallback.js
@@ -0,0 +1,25 @@
+import React, { useState, useCallback } from 'react'
+import { ItemsList } from './ItemsList'
+
+export const App = () => {
+  const [colored, setColored] = useState(false)
+  const [count, setCount] = useState(1)
+
+  const styles = {
+    color: colored ? 'darkred' : 'black'
+  }
+
+  const generateItemsFromAPI = useCallback((indexNumber) => {
+    return new Array(count).fill('').map((_, i) => `Element ${i + indexNumber}`)
+  }, [count])
+
+  return (
+    <>
+      <h1 style={styles}>Count elements: {count}</h1>
+      <button className={'btn btn-success'} onClick={() => setCount(prev => prev + 1)}>Add</button>
+      <button className={'btn btn-warning'} onClick={() => setColored(prev => !prev)}>Change</button>
+
+      <ItemsList getItems={generateItemsFromAPI} />
+    </>
+  )
+}
