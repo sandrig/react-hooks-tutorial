6fa88f1d9bc26320ad8e0b9425bce3dbe383543c
sandrig <sandrig.dev@gmail.com>
2020-08-26 19:15:59 +0300

    feat: add useMemo hook example

---
+++ src/examples/useMemo.js
@@ -0,0 +1,34 @@
+import React, { useState, useMemo, useEffect } from 'react'
+
+const complexCompute = num => {
+  console.log('complexCompute')
+  let i = 0
+  while (i < 1000000000) i++
+  return num * 2
+}
+
+export const App = () => {
+  const [number, setNumber] = useState(42)
+  const [colored, setColored] = useState(false)
+
+  const styles = useMemo(() => ({
+    color: colored ? 'darkred' : 'black'
+  }), [colored])
+
+  const computed = useMemo(() => {
+    return complexCompute(number)
+  }, [number])
+
+  useEffect(() => {
+    console.log('Styles changed')
+  }, [styles])
+
+  return (
+    <>
+      <h1 style={styles}>Computed property: {computed}</h1>
+      <button className={'btn btn-success'} onClick={() => setNumber(prev => prev + 1)}>Add</button>
+      <button className={'btn btn-danger'} onClick={() => setNumber(prev => prev - 1)}>Delete</button>
+      <button className={'btn btn-warning'} onClick={() => setColored(prev => !prev)}>Change</button>
+    </>
+  )
+}
