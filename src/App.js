4454e96389704369e7ceca99d2d346e56eb09bd7
sandrig <sandrig.dev@gmail.com>
2020-08-27 10:21:29 +0300

    feat: add custom hook example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
-import React from 'react'
-import { Main } from './Main'
-import { Alert } from './alert/Alert'
-import { AlertProvider } from './alert/AlertContext'
+import React, {useEffect, useState} from 'react'
+
+const useLogger = value => {
+  useEffect(() => {
+    console.log('Value changed', value)
+  }, [value])
+}
+
+const useInput = initialValue => {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = event => {
+    setValue(event.target.value)
+  }
+
+  const clear = () => setValue('')
+
+  return {
+    bind: { value, onChange },
+    value,
+    clear
+  }
+}
 
 export const App = () => {
+  const input = useInput('')
+
+  // const [name, setName] = useState()
+  // const [lastName, setLastName] = useState()
+
+  // const changeHandler = event => {
+  //   setName(event.target.value)
+  // }
+
+  // const lastNameHandler = event => {
+  //   setLastName(event.target.value)
+  // }
+
+  // useLogger(name)
+  useLogger(input.value)
+
   return (
-    <AlertProvider>
-      <div className={'container pt-3'}>
-        <Alert />
-        <Main toggle={() => {}} />
-      </div>
-    </AlertProvider>
+    <div className={'container pt-3'}>
+      <input type="text" {...input.bind} />
+      <button className="btn btn-warning" onClick={() => input.clear()}>Clear</button>
+      {/*<input type="text" value={input.value} onChange={input.onChange} />*/}
+      {/*<input type="text" value={name} onChange={changeHandler} />*/}
+      {/*<input type="text" value={lastName} onChange={lastNameHandler} />*/}
+      <hr />
+      {/*<h1>{name} {lastName}</h1>*/}
+      {/*<h1>{name}</h1>*/}
+      <h1>{input.value}</h1>
+    </div>
   )
 }
 
